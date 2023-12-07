c47a5a5f9e3b73ea5736c707a0c20997ec9dec36
Roser Fuster Bertomeu <rfusterb@uoc.edu>
2023-12-07 08:54:45 +0100

    exercici 3 + estils

--- src/app/Shared/Components/spinner/spinner.component.ts
+++ src/app/Shared/Components/spinner/spinner.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AppState } from 'src/app/app.reducers';
 
 @Component({
@@ -8,24 +10,18 @@ import { AppState } from 'src/app/app.reducers';
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
+  loading_auth$: Observable<boolean>;
+  loading_user$: Observable<boolean>;
+  loading_categories$: Observable<boolean>;
+  loading_posts$: Observable<boolean>;
   constructor(
     private store: Store<AppState>
-  ) {}
+  ) {
+    this.loading_auth$ = this.store.select('auth').pipe(map(auth => auth.loading));
+    this.loading_user$ = this.store.select('user').pipe(map(user => user.loading));
+    this.loading_categories$ = this.store.select('categories').pipe(map(categories => categories.loading));
+    this.loading_posts$ = this.store.select('posts').pipe(map(posts => posts.loading));
 
-  loading_auth$ = this.store.select('auth').subscribe((auth) => {
-    auth.loading
-   });
- 
-   loading_user$ = this.store.select('user').subscribe((user) => {
-     user.loading
-    });
- 
-   loading_categories$ = this.store.select('categories').subscribe((categories) => {
-     categories.loading;
-   });
- 
-   loading_posts$ = this.store.select('posts').subscribe((posts) => {
-     posts.loading;
-   });
+  }
 }
 
